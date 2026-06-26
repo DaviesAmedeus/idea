b19ec4db004739301b2d660615a9c303a738c1b2
DaviesAmedeus <amedeusedgar@gmail.com>
2026-06-26 12:50:22 +0300

    Tailwind Theme Setup And Initial UI

---
+++ app/Http/Controllers/SessionsController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class SessionsController extends Controller
+{
+    public function create()
+    {
+        return view('auth.login');
+    }
+
+     public function store(Request $request)
+    {
+        $creds = $request->validate([
+              'email'=>['required', 'string', 'min:3', 'max:255'],
+        'password'=>['required', 'string', 'min:8', 'max:255'],
+
+        ]);
+
+        if(!Auth::attempt($creds)){
+            return redirect()->back()->withErrors([
+                'password'=> 'we were unable to aunthenticate using the provided credentials'
+            ])->withInput();
+        }
+
+        $request->session()->regenerate();
+        return redirect()->intended('/')->with('success','You are logged in');
+
+
+
+    }
+
+    public function destroy()
+    {
+        Auth::logout();
+        return redirect('/');
+    }
+}
